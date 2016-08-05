39911394173fc5e207a0c4050f15c7decbe49cdf
Simone Guercio <s.guercio@gmail.com>
2016-08-05 20:14:32 +0200

    Add books and grades table
    Handled the many-to-many relation.
    
    The Book.from_file() now catches the exception if trying
    to add the same book twice (check via EAN) and handles it
    graciously.

--- gestionale.py
+++ gestionale.py
@@ -5,6 +5,7 @@ import csv
 from flask import Flask, render_template
 from flask_script import Manager
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import exc
 
 basedir = os.path.abspath(os.path.dirname('.'))
 
@@ -14,6 +15,15 @@ db = SQLAlchemy(app)
 manager = Manager(app)
 
 
+#----------------------------
+# Tabelle Helpers Database
+#----------------------------
+adoptions = db.Table('adoptions',
+                     db.Column('book_id', db.Integer, db.ForeignKey('books.id')),
+                     db.Column('grade_id', db.Integer, db.ForeignKey('grades.id'))
+                     )
+
+
 #----------------------
 #  Classi Database
 #----------------------
@@ -21,10 +31,11 @@ class Book(db.Model):
     __tablename__ = 'books'
     id = db.Column(db.Integer, primary_key = True)
     title = db.Column(db.Text, nullable = False)
-    ean = db.Column(db.Text, nullable = False)
+    ean = db.Column(db.Text, nullable = False, unique = True)
     author = db.Column(db.Text, nullable = False)
     subject = db.Column(db.Text, nullable = False)
     publisher = db.Column(db.Text, nullable = False)
+    adopted_by = db.relationship('Grade', secondary = adoptions, backref = db.backref('adopted_books', lazy = 'dynamic'))
 
     def __init__(self, title, ean, author = 'default', subject = 'default', publisher = 'default'):
         self.title = title
@@ -34,18 +45,29 @@ class Book(db.Model):
         self.publisher = publisher
 
     @classmethod
-    def from_file(self, filename = 'data.csv'):
+    def from_file(cls, filename = 'data.csv'):
         with open(filename, newline = '') as csvfile:
             csvreader = csv.DictReader(csvfile, fieldnames = ('title', 'ean', 'author', 'subject', 'publisher'))
             for row in csvreader:
-                db.session.add(Book(row['title'], row['ean'], row['author'], row['subject'], row['publisher']))
+                try:
+                    db.session.add(cls(row['title'], row['ean'], row['author'], row['subject'], row['publisher']))
+                    db.session.commit()
+                except exc.IntegrityError:
+                    db.session.rollback()
                 
 
+
+class Grade(db.Model):
+    __tablename__ = 'grades'
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.Text, nullable = False)
+
+    def __init__(self, grade):
+        self.name = classe
 
 #------------
 #  Routes
 #------------
-
 @app.route('/')
 def index():
     return render_template('index.html',
